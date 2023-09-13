aa4c7905a10bb7d64e4ce9bf9e5af190c3d09340
Alzate <luismiguelalzate421@gmail.com>
2023-09-13 12:45:58 -0500

    Complete Contact and install emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
         <Services />
         <Qualification />
         <Testimonials />
+        <Contact />
       </main>
     </>
   )
